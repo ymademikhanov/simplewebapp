46ace2457b0641b1407f468fae07986172c57f8c
ymademikhanov <mademihanov.erzhan@gmail.com>
2018-02-14 15:47:43 +0600

    Add id to Todo class objects. Id increases automatically whenever class object is created.

--- src/main/Todo.java
+++ src/main/Todo.java
@@ -1,10 +1,13 @@
 package main;
 
 public class Todo {
+	static int counter = 0;
+	private int id;
 	private String description;
 	private String title;
 	
 	public Todo(String title, String description) {
+		this.id = ++counter;
 		this.title = title;
 		this.description = description;
 	}

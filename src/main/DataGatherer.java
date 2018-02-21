5575addb48dc5da9765b5553a58d13ce5b25d85f
ymademikhanov <mademihanov.erzhan@gmail.com>
2018-02-21 10:56:29 +0600

    Added DataGatherer and modified ToDo class.

---
+++ src/main/DataGatherer.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.google.gson.Gson;
+
+public class DataGatherer {
+	static private int counter = 0;
+	private Gson gson = new Gson();
+	private Map<Integer, Todo> listMap = new HashMap<Integer, Todo>();
+	
+	public String getCollection() {
+		return gson.toJson(listMap);
+	}
+	
+	public String getRow(Integer id) {
+		return gson.toJson(listMap.get(id));
+	}
+	
+	public void addRow(String json) {
+		Todo todo = gson.fromJson(json, Todo.class);
+		todo.setId(++counter);
+		listMap.put(todo.getId(), todo);
+	}
+}
--- src/main/Todo.java
+++ src/main/Todo.java
@@ -1,15 +1,17 @@
 package main;
 
 public class Todo {
-	static private int counter = 0;
 	private int id;
 	private String description;
 	private String title;
 	
 	public Todo(String title, String description) {
-		this.id = ++counter;
 		this.title = title;
 		this.description = description;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
 	}
 	
 	public int getId() {
